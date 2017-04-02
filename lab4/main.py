38311e2be53fa5d655f993daffaa59d9d9aac267
Sych <rk474@yandex.ru>
2017-04-02 23:36:57 +0300

    Lab4: add part 2

--- lab4/main.py
+++ lab4/main.py
@@ -49,6 +49,98 @@ def alignment_variables(table, h):
     return ans
 
 
+def get_points_to_interpolation(point_list, x, n):
+    # предполагается, что point_list отсортирован, и в нем как минимум n+1 точка
+    x_ind = 0
+    while x_ind < len(point_list) and point_list[x_ind].x < x:
+        x_ind += 1
+    lower_ind = x_ind - n // 2 - n % 2  # если нечетное, то вниз берем больше.
+    upper_ind = x_ind + n // 2
+    if lower_ind < 0:
+        upper_ind += abs(lower_ind)
+        lower_ind = 0
+    elif upper_ind >= len(point_list):
+        lower_ind -= upper_ind - (len(point_list) - 1)
+        upper_ind = len(point_list) - 1
+    return [point_list[i] for i in range(lower_ind, upper_ind + 1)]
+
+
+def a_progression(a, b, n):
+    return (a+b) *n // 2
+
+
+def get_split_diffs(points_to_interpolation, n):
+    split_diffs = [(points_to_interpolation[i].y if i < n+1 else 0)
+                   for i in range(a_progression(a=1, b=n+1, n=n+1))]
+    ind = n+1
+    for i in range(n):
+        offset = (n - 1 - i)
+        for j in range(n-i):
+            dy = split_diffs[ind - offset - 2] - split_diffs[ind - offset - 1]
+            dx = points_to_interpolation[j].x - points_to_interpolation[j+i+1].x
+            split_diffs[ind] = dy/dx
+            ind += 1
+    return split_diffs
+
+
+def combinations(iterable, r):
+    # combinations('ABCD', 2) --> AB AC AD BC BD CD
+    # combinations(range(4), 3) --> 012 013 023 123
+    pool = tuple(iterable)
+    n = len(pool)
+    if r > n:
+        return
+    indices = list(range(r))
+    yield tuple(pool[i] for i in indices)
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != i + n - r:
+                break
+        else:
+            return
+        indices[i] += 1
+        for j in range(i+1, r):
+            indices[j] = indices[j-1] + 1
+        yield tuple(pool[i] for i in indices)
+
+
+def mult(combination):
+    ans = 1
+    for elem in combination:
+        ans *= float(elem)
+    return ans
+
+
+def newton_polinom(table, x, n):
+    if len(table) < n + 1:
+        return None
+    interp_points = get_points_to_interpolation(point_list=table, x=x, n=n)
+    #print(interp_points)
+    split_diffs = get_split_diffs(points_to_interpolation=interp_points, n=n)
+    #print(split_diffs)
+    z = [x - interp_points[i].x for i in range(len(interp_points))]
+    #print(z)
+    ind = n+1
+    y = split_diffs[ind]
+    for i in range(1, n):
+        comb = combinations(z[:i+1], i)
+        #comb_a = combinations([str(i) for i in range(i+1)], i)
+        #for elem in comb_a:
+        #    print(elem)
+
+        ind += n - i + 1
+        add = split_diffs[ind]
+        tmp = 0
+        for elem in comb:
+        #    print(elem)
+            tmp += mult(elem)
+        #print(tmp)
+        add *= tmp
+        #print(add)
+        y += add
+    return y
+
+
 def get_table(f, a, b, h):
     table = list()
     x = a
@@ -59,24 +151,21 @@ def get_table(f, a, b, h):
 
 
 def print_table(table, r_newton, m_newton, h_perc, runge, alignment):
-    print("|{:7s}|{:7s}|{:7s}|{:7s}|{:7s}|{:7s}|{:7s}|".format("x", "y", "Правый", "Среднее", "Повыш.", "Рунге", "Выравн."))
+    print("|{:10s}|{:10s}|{:10s}|{:10s}|{:10s}|{:10s}|{:10s}|".format("x", "y", "Правый", "Среднее", "Повыш.", "Рунге", "Выравн."))
     for i in range(len(table)):
-        s1 = "{:7.4f}".format(r_newton[i]) if r_newton[i] else ' '
-        s2 = "{:7.4f}".format(m_newton[i]) if m_newton[i] else ' '
-        s3 = "{:7.4f}".format(h_perc[i]) if h_perc[i] else ' '
-        s4 = "{:7.4f}".format(runge[i]) if runge[i] else ' '
-        s5 = "{:7.4f}".format(alignment[i]) if alignment[i] else ' '
-        print("|{:7.4f}|{:7.4f}|{:7s}|{:7s}|{:7s}|{:7s}|{:7s}|".format(table[i].x, table[i].y, s1, s2, s3, s4, s5))
-
-
-
+        s1 = "{:10.4f}".format(r_newton[i]) if r_newton[i] else ' '
+        s2 = "{:10.4f}".format(m_newton[i]) if m_newton[i] else ' '
+        s3 = "{:10.4f}".format(h_perc[i]) if h_perc[i] else ' '
+        s4 = "{:10.4f}".format(runge[i]) if runge[i] else ' '
+        s5 = "{:10.4f}".format(alignment[i]) if alignment[i] else ' '
+        print("|{:10.4f}|{:10.4f}|{:10s}|{:10s}|{:10s}|{:10s}|{:10s}|".format(table[i].x, table[i].y, s1, s2, s3, s4, s5))
 
 
 def main():
-    a = 1
-    b = 3
-    #h = float(input("Введите шаг: "))
-    h = 0.5
+    a = float(input("Введите a: "))
+    b = float(input("Введите b: "))
+    h = float(input("Введите шаг: "))
+
     table = get_table(f=f, a=a, b=b, h=h)
     r_n = right_newton(table=table, h=h)
     m_n = middle_newton(table=table, h=h)
@@ -84,6 +173,11 @@ def main():
     r = runge(table=table, h=h)
     a_v = alignment_variables(table=table, h=h)
     print_table(table=table, r_newton=r_n, m_newton=m_n, h_perc=h_p, runge=r, alignment=a_v)
+
+    x = float(input("Введите x: "))
+    n = int(input("Введите число узлов: "))
+    print(newton_polinom(table=table, x=x, n=n-1))
+    print()
 
 
 if __name__ == '__main__':
