ec5145d698369e22a5aa2c0cd25229b04cd313fa
Sych <rk474@yandex.ru>
2017-04-16 13:12:24 +0300

    Lab5: add definitions and imp of some functions

--- lab5/main.py
+++ lab5/main.py
@@ -1,3 +1,16 @@
+from math import sqrt, pi, e
+
+
+def gauss_integral(a, b, f, n):
+    return 0
+
+
+def ft(t):
+    return e ** (-t*t/2)
+
+
+def get_integral(x, n):
+    return 1/sqrt(2*pi) * gauss_integral(0, x, ft, n)
 
 
 def main():
