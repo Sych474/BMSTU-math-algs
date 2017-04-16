a50345262ab42b2e609b565ce5b538316dec5fd9
Sych <rk474@yandex.ru>
2017-04-16 20:52:05 +0300

    Lab5: add Legendre_roots

--- lab5/main.py
+++ lab5/main.py
@@ -1,7 +1,35 @@
-from math import sqrt, pi, e
+from math import sqrt, pi, e, cos
+
+EPS = 10**-10
+
+
+def P(n, x):
+    if n == 0:
+        return 1
+    if n == 1:
+        return x
+    return (2*(n-1)+1)/((n-1)+1) * x * P(n-1, x) - (n-1)/((n-1)+1) * P(n-2, x)
+
+
+def P_derivative(n, x):
+    return n / (1 - x*x) * (P(n-1, x) - x*P(n, x))
+
+
+def legendre_roots(n):
+    roots = list()
+
+    for i in range(1, n+1):
+        x_old = cos(pi*(4*i - 1)/(4*n + 2))
+        x_new = x_old - P(n=n, x=x_old)/P_derivative(n=n, x=x_old)
+
+        while abs(x_new - x_old)/x_old >= EPS:
+            x_old, x_new = x_new, x_new - P(n=n, x=x_new)/P_derivative(n=n, x=x_new)
+        roots.append(x_new)
+    return roots
 
 
 def gauss_integral(a, b, f, n):
+    roots = legendre_roots(n=n)
     return 0
 
 
@@ -10,7 +38,7 @@ def ft(t):
 
 
 def get_integral(x, n):
-    return 1/sqrt(2*pi) * gauss_integral(0, x, ft, n)
+    return 1/sqrt(2*pi) * gauss_integral(a=0, b=x, f=ft, n=n)
 
 
 def main():
